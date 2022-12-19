39375b4e5eda40c27ad5382aac5febe5569be96b
likhith_harish <62323090+likhithharish@users.noreply.github.com>
2022-12-19 17:02:55 -0500

    Time: 1057 ms (5.04%), Space: 145.8 MB (5.16%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -0,0 +1,40 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        List<List<String>> results = new ArrayList<List<String>>();
+        List<Integer> done = new ArrayList<>();
+        
+        for( int i=0;i<strs.length;i++){
+            List<String> temp = new ArrayList<>();
+            if(!done.contains(i)){
+                temp.add(strs[i]);
+                for( int j=i+1;j<strs.length;j++){
+                    if(isAnagram(strs[i],strs[j])){
+                        done.add(i);
+                        done.add(j);
+                        temp.add(strs[j]);
+                    }
+                }
+            }
+            if(temp.size()!=0){
+                results.add(temp);
+            }
+        }
+        return results;
+    }
+    
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()){
+            return false;
+        }
+        
+        int[] count = new int[26];
+        for( int i=0; i<s.length() ; i++){
+            count[s.charAt(i)-'a']++;
+            count[t.charAt(i)-'a']--;
+        }
+        
+        for( int i: count){ if(i!=0){return false;} }
+        
+        return true;
+    }
+}
