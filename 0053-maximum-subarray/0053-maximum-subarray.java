0afcbe83c61fa95f2eda33eee1a1571a46caccb3
likhith_harish <62323090+likhithharish@users.noreply.github.com>
2023-01-08 21:54:54 -0500

    Time: 1 ms (100.00%), Space: 51.7 MB (74.70%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,31 +1,31 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-//         int sum =Integer.MIN_VALUE, tempSum=0;
-//         for(int i=0;i<nums.length;i++){
-//             tempSum = tempSum +nums[i];
-//             if(tempSum > sum){
-//                 sum = tempSum;
+        int sum =Integer.MIN_VALUE, tempSum=0;
+        for(int i=0;i<nums.length;i++){
+            tempSum = tempSum +nums[i];
+            if(tempSum > sum){
+                sum = tempSum;
                 
-//             }
-//             if(tempSum <0){
-//                 tempSum =0;
-//             }
-            
-//         }
-//         return sum;
-        int temp[] = new int[nums.length];
-        temp[0] = nums[0];
-        for( int i=1; i< nums.length; i++){
-            //int res = Math.max(temp[i-1],nums[i]);
-            int res = Math.max(temp[i-1]+nums[i], nums[i]);
-            temp[i] = res;
-        }
-        int max = temp[0];
-        for( int i: temp){
-            if(i> max){
-                max = i;
             }
+            if(tempSum <0){
+                tempSum =0;
+            }
+            
         }
-        return max;
+        return sum;
+        // int temp[] = new int[nums.length];
+        // temp[0] = nums[0];
+        // for( int i=1; i< nums.length; i++){
+        //     //int res = Math.max(temp[i-1],nums[i]);
+        //     int res = Math.max(temp[i-1]+nums[i], nums[i]);
+        //     temp[i] = res;
+        // }
+        // int max = temp[0];
+        // for( int i: temp){
+        //     if(i> max){
+        //         max = i;
+        //     }
+        // }
+        // return max;
     }
 }
