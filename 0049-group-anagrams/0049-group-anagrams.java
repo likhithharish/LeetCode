8137a38752980ec9f8a8526371eeb662b008b536
likhith_harish <62323090+likhithharish@users.noreply.github.com>
2023-01-24 04:19:56 -0500

    Time: 25 ms (22.02%), Space: 46.3 MB (50.58%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,60 +1,24 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        //List<List<String>> results = new ArrayList<List<String>>();
-        /*List<Integer> done = new ArrayList<>();
-        
-        for( int i=0;i<strs.length;i++){
-            List<String> temp = new ArrayList<>();
-            if(!done.contains(i)){
-                temp.add(strs[i]);
-                for( int j=i+1;j<strs.length;j++){
-                    if(isAnagram(strs[i],strs[j])){
-                        done.add(i);
-                        done.add(j);
-                        temp.add(strs[j]);
-                    }
-                }
-                results.add(temp);
-            }
-        }
-        return results;*/ // Basic Approach
-        
-        //Better Solution 
-        Map<String, List> results = new HashMap<String,List>();
-        for(String s : strs){
+        HashMap<String,List> map = new HashMap<>();
+        for( String s: strs){
             int[] count = new int[26];
-            for( int i=0; i<s.length() ; i++){
+            for( int i=0; i< s.length();i++){
                 count[s.charAt(i)-'a']++;
+                
             }
-            StringBuilder str = new StringBuilder("");
-            for (int i = 0; i < 26; i++) {
-                str.append('#');
+            StringBuilder str = new StringBuilder();
+            for( int i=0; i< 26; i++){
+                str.append("#");
                 str.append(count[i]);
             }
             
-            if(!results.containsKey(str.toString())){
-                results.put(str.toString(), new ArrayList());
+            if(!map.containsKey(str.toString())){
+                map.put(str.toString(), new ArrayList());
             }
-            results.get(str.toString()).add(s);
+            map.get(str.toString()).add(s);
+            
         }
-        
-        return new ArrayList(results.values());
-     
-    }
-    
-    public boolean isAnagram(String s, String t) {
-        if(s.length() != t.length()){
-            return false;
-        }
-        
-        int[] count = new int[26];
-        for( int i=0; i<s.length() ; i++){
-            count[s.charAt(i)-'a']++;
-            count[t.charAt(i)-'a']--;
-        }
-        
-        for( int i: count){ if(i!=0){return false;} }
-        
-        return true;
+        return new ArrayList(map.values());
     }
 }
