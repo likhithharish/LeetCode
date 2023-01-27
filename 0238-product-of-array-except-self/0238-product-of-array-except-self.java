ab88e31b805d51ffc552377507232b18cd9fc3d1
likhith_harish <62323090+likhithharish@users.noreply.github.com>
2023-01-26 21:16:43 -0500

    Time: 2 ms (56.99%), Space: 57.8 MB (21.88%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,41 +1,37 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int product,start;
-        if(nums[0]!=0){
-            start =1;
-            product =nums[0];
-        }else{
-            start =0;
-            product =1;
-        }
-        int zeroCount =0;
-        boolean zero = false;
-        
-        for( int i=start; i< nums.length; i++){
+        int zeroCount = 0;
+        int product =1;
+        for( int i=0; i<nums.length; i++){
             if(nums[i]!=0){
-                product = product * nums[i];
-                
-            }else{
-                zero = true;
+                product *= nums[i];
+            }
+            if(nums[i]==0){
                 zeroCount++;
             }
         }
         
-        if(zeroCount>1){
-            for( int i=0;i<nums.length;i++){
-                nums[i]=0;
+        if(zeroCount ==1){
+            for( int i=0; i< nums.length; i++){
+                if(nums[i]==0){
+                    nums[i] = product;
+                }else{
+                    nums[i]=0;
+                }
             }
+            return nums;
+        }else if (zeroCount >=2){
+            for( int i=0; i<nums.length; i++){
+                nums[i] =0;
+            }
+            return nums;
         }else{
-            for( int i=0;i<nums.length;i++){
-                if(zero & nums[i]!=0){
-                nums[i]=0;
-            }else if ( zero & nums[i]==0){
-                nums[i]= product;
-            }else{
-                nums[i] = product/nums[i];
-            }
+            for( int i=0; i<nums.length; i++){
+                nums[i] = product / nums[i];
             }
         }
+        
         return nums;
+        
     }
 }
