8535e6103924728b3f480b8ac96cee33e1d244fd
Likhith Harish <62323090+likhithharish@users.noreply.github.com>
2023-04-17 05:12:34 -0400

    Time: 2 ms (58.46%), Space: 46.9 MB (43.10%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -1,19 +1,23 @@
 class Solution {
+    int[] tbl = new int[256];
+    void initialize(){
+        tbl[0] = 0;
+        for (int i = 1; i < 256; i++) {
+            tbl[i] = tbl[i & (i - 1)] + 1;
+        }
+    }
     public int[] countBits(int n) {
         int[] res = new int[n+1];
         res[0] =0;
+        initialize();
         for( int i=1; i<=n; i++){
             res[i] = countBitsInNum(i);
         }
         return res;
     }
     
-    public int countBitsInNum( int num){
-        int res =0;
-        while(num>0){
-            num = num & (num-1);
-            res = res +1;
-        }
-        return res;
+    public int countBitsInNum( int n){
+        //Loopup Table approach:
+        return tbl[n & 255] + tbl[(n >> 8) & 255] + tbl[(n >> 16) & 255] + tbl[n >> 24];
     }
 }
