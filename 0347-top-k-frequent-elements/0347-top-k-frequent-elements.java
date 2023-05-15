8880633f210138aa1afd0c85996f3080fb6dcf08
Likhith Harish <62323090+likhithharish@users.noreply.github.com>
2023-05-15 04:03:02 -0400

    Time: 13 ms (80.71%), Space: 45 MB (88.94%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,24 +1,28 @@
 class Solution {
+    /*
+    Intuition::
+    - Use a hashmap to count the array elements .
+    - Use a priority queue to sort the map and keep map key,value pairs in pq.
+    - loop through priority queue and get top K elements from pq.
+    */
     public int[] topKFrequent(int[] nums, int k) {
         HashMap<Integer, Integer> map = new HashMap<>();
-        
         for( int i: nums){
             map.put(i, map.getOrDefault(i,0)+1);
         }
         
-        PriorityQueue<Map.Entry<Integer,Integer>> pq 
-            = new PriorityQueue<>((a,b) -> b.getValue()-a.getValue());
+    PriorityQueue<Map.Entry<Integer,Integer>> pq = new PriorityQueue<>((a,b)->b.getValue() - a.getValue());
         
         for(Map.Entry entry : map.entrySet()){
             pq.add(entry);
         }
         
-        int[] out = new int[k];
-        for(int i=0;i<k; i++){
-            out[i] = pq.poll().getKey();
+        int[] res = new int[k];
+        for( int i=0; i< k; i++){
+            res[i] =pq.poll().getKey();
         }
-        return out;
         
+        return res;
         
     }
 }
