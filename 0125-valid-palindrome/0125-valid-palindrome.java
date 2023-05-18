43f41fb6328c74264d70ca91bb969db7c80a4c0a
Likhith Harish <62323090+likhithharish@users.noreply.github.com>
2023-05-18 03:26:19 -0400

    Time: 25 ms (38.86%), Space: 43.4 MB (38.34%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,4 +1,8 @@
 class Solution {
+    /*
+    Intuition:
+    - Start from both ends and compare each character and if not same return false.
+    */
     public boolean isPalindrome(String s) {
         s = s.replaceAll("[^a-zA-Z0-9]","").toLowerCase();
         int low =0, high= s.length()-1;
