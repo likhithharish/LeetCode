008a8e5262c033cddf2291221c52d038fef3589a
Likhith Harish <62323090+likhithharish@users.noreply.github.com>
2023-05-28 06:56:37 -0400

    Time: 0 ms (100.00%), Space: 41 MB (79.53%) - LeetHub

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -1,4 +1,8 @@
 class Solution {
+    /*
+    Intuition:
+    - Create copy of initial array and move elements to initial array if its not equal to val , and increment count.
+    */
     public int removeElement(int[] nums, int val) {
         int k=0,valCount=0;
         int[] copy = Arrays.copyOf(nums,nums.length);
