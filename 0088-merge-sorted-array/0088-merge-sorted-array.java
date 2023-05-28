75978467ad7119501868f8bcd86c5b53da89de9b
Likhith Harish <62323090+likhithharish@users.noreply.github.com>
2023-05-28 06:43:28 -0400

    Time: 0 ms (100.00%), Space: 41.7 MB (89.16%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,8 +1,42 @@
 class Solution {
+    /*
+    Intuition:
+    - Naive : Merge two arrays and perform sort.
+    - Two Pointer :Make new array copy of nums1 , move values to nums1 by comparing num2 and copy array .
+    */
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        for( int i=m; i<nums1.length; i++){
-            nums1[i] = nums2[i-m];
+        //Naive Solution 
+        // for( int i=m; i<nums1.length; i++){
+        //     nums1[i] = nums2[i-m];
+        // }
+        // Arrays.sort(nums1);
+        int[] copy = new int[m];
+        for( int i=0; i<m; i++){
+            copy[i] = nums1[i];
         }
-        Arrays.sort(nums1);
+        int p1 = 0;
+        int pc1 = 0, p2=0;
+        while(pc1<m && p2<n){
+            if(copy[pc1]<=nums2[p2]){
+                nums1[p1] = copy[pc1];
+                pc1++;           
+            }else if(copy[pc1]>nums2[p2]){
+                nums1[p1] = nums2[p2];
+                p2++;  
+            }
+            p1++;
+        }
+        if(pc1!=m){
+            //move rest of copy to num1
+            for( int i=p1; i<(m+n); i++){
+                nums1[i] = copy[pc1];
+                pc1++;
+            }
+        }else{
+            for( int i=p1; i<(m+n); i++){
+                nums1[i] = nums2[p2];
+                p2++;
+            }
+        }
     }
 }
