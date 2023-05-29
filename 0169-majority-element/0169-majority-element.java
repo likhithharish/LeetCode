cd883538015be255650e27e7b803c8eba1840398
Likhith Harish <62323090+likhithharish@users.noreply.github.com>
2023-05-29 07:31:08 -0400

    Time: 1 ms (99.60%), Space: 48.6 MB (9.86%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -3,24 +3,40 @@ class Solution {
     Basic Intuition : Sort and find the majority element
     */
     public int majorityElement(int[] nums) {
-        Arrays.sort(nums);
-        int currElem = nums[0];
-        int maxElem = nums[0];
-        int maxCount =1;
-        int res = 0;
-        for( int i=1; i< nums.length; i++){
-            if(nums[i] == currElem){
-                maxCount++;
-            }else{
-                currElem = nums[i];
-                maxCount =1;
-            }
-            if(maxCount> res){
-                maxElem = nums[i];
-                res = maxCount;
-            }
+//         Arrays.sort(nums);
+//         int currElem = nums[0];
+//         int maxElem = nums[0];
+//         int maxCount =1;
+//         int res = 0;
+//         for( int i=1; i< nums.length; i++){
+//             if(nums[i] == currElem){
+//                 maxCount++;
+//             }else{
+//                 currElem = nums[i];
+//                 maxCount =1;
+//             }
+//             if(maxCount> res){
+//                 maxElem = nums[i];
+//                 res = maxCount;
+//             }
             
+//         }
+//         return maxElem;
+       //Boyer -Moore Voting Algorithm -NOTES
+        int count =1;
+        int res = nums[0];
+        for ( int i=1; i<nums.length ; i++){
+            if(res == nums[i]){
+                count++;
+            }else{
+                count--;
+                
+            }
+            if(count <0 ){
+                res = nums[i];
+                count =1;
+            }
         }
-        return maxElem;
+        return res;
     }
 }
