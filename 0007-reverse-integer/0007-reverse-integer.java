a0af41a34e4de1794f6d2e896a0e55bb4ea88ca2
Likhith Harish <62323090+likhithharish@users.noreply.github.com>
2023-05-31 10:06:20 -0400

    Time: 1 ms (100.00%), Space: 40.1 MB (49.69%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.java
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -1,27 +1,24 @@
 class Solution {
+    /*Intuition:
+    - Reverse the number is logic is same for both positive and negative numbers.
+    - Issue is to handle the reversed number doesnt go beyond the 32 bit bounds.
+    - Have to do this before adding the last digit . So we check if current reveresed number is equal to Integer max and min values(without last digits) and compare accordingly with lastDigit also.
+    */
     public int reverse(int x) {
-//         int num = 0;
-//         while( x!=0){
-//             int lastDigit = x %10;
-//             if(num<Integer.MIN_VALUE/10 && (num == Integer.MIN_VALUE/10 && lastDigit< -8)){
-//                 return 0;
-//             }
-//             if( num> Integer.MAX_VALUE/10 && (num == Integer.MAX_VALUE/10 && lastDigit>7)){
-//                 return 0;
-//             }
-//             num = num * 10 + lastDigit;
-//             x = x/10;
-//         }
-        
-//         return num;
-        int rev = 0;
-        while (x != 0) {
-            int pop = x % 10;
-            x /= 10;
-            if (rev > Integer.MAX_VALUE/10 || (rev == Integer.MAX_VALUE / 10 && pop > 7)) return 0;
-            if (rev < Integer.MIN_VALUE/10 || (rev == Integer.MIN_VALUE / 10 && pop < -8)) return 0;
-            rev = rev * 10 + pop;
+        int num = 0;
+        while( x!=0){
+            int lastDigit = x %10;
+            if(num<Integer.MIN_VALUE/10 || (num == Integer.MIN_VALUE/10 && lastDigit< -8)){
+                return 0;
+            }
+            if( num> Integer.MAX_VALUE/10 || (num == Integer.MAX_VALUE/10 && lastDigit>7)){
+                return 0;
+            }
+            num = num * 10 + lastDigit;
+            x = x/10;
         }
-        return rev;
+        
+        return num;
+        
     }
 }
