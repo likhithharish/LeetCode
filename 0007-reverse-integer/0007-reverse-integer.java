143fa0a55332d7398f890d6449af409975e647c4
Likhith Harish <62323090+likhithharish@users.noreply.github.com>
2023-05-31 09:41:55 -0400

    Time: 1 ms (100.00%), Space: 39.9 MB (61.98%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.java
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -1,27 +1,41 @@
 class Solution {
     public int reverse(int x) {
-        boolean symbol =false;
-        if(x<0){
-            x =0 -x;
-            symbol = true;
-        }
-        int n = x;
-        int count =0;
-        while(n>0){
-            n = n/10;
-            count++;
-        }
+//         boolean symbol =false;
+//         if(x<0){
+//             x =0 -x;
+//             symbol = true;
+//         }
+//         int n = x;
+//         int count =0;
+//         while(n>0){
+//             n = n/10;
+//             count++;
+//         }
         
-        long num = 0;
-        while(x>0){
-            int digit = x %10;
+//         long num = 0;
+//         while(x>0){
+//             int digit = x %10;
+//             x = x/10;
+//             num = num + (int) (digit * Math.pow(10,(count-1)));
+//             count--;
+//         }
+//         if(num>Integer.MAX_VALUE || num<Integer.MIN_VALUE){
+//             return 0;
+//         }
+//         return (symbol) ? (int) -num : (int) num;
+        //Improvised:
+        long num =0;
+        while(x != 0){
+            int digit = x%10;
+            num = num * 10 + digit;
             x = x/10;
-            num = num + (int) (digit * Math.pow(10,(count-1)));
-            count--;
+            
         }
         if(num>Integer.MAX_VALUE || num<Integer.MIN_VALUE){
             return 0;
         }
-        return (symbol) ? (int) -num : (int) num;
+        if(x<0){ num = -num;}
+        return (int) num;
+        
     }
 }
