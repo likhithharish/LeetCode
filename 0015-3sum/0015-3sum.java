5bf9d9adf7d9d03924343b1790a255f6396aedf6
Likhith Harish <62323090+likhithharish@users.noreply.github.com>
2023-06-01 05:22:46 -0400

    Time: 37 ms (87.70%), Space: 50.3 MB (50.44%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,14 +1,15 @@
 class Solution {
     /*
     Intuition:
-    - Sort the array , and loop through .
+    - Sort the array , and loop till the positive element only.
     - to avoid duplicates , check if prev number is not same as curr .
+    - if target found, check if next start and end indexes are duplicates and avoid them.(two whiles)
+    - if target greater, decrement end index. else increment start index.
     */
     public List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
         Arrays.sort(nums);
-        // -4, -1, -1, 0 , 1, 2
-        for( int i=0; i<nums.length; i++){
+        for( int i=0; i<nums.length && nums[i]<=0; i++){
             int target = 0- nums[i];
             if(i==0 || (i>0 && nums[i] != nums[i-1])){
                 int start = i+1;
