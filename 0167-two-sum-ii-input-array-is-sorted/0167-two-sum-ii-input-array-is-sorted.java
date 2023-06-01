05c5e6310875b0daa453a2245ef7aad1537bf505
Likhith Harish <62323090+likhithharish@users.noreply.github.com>
2023-06-01 02:01:11 -0400

    Time: 1 ms (99.17%), Space: 45.7 MB (24.51%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -1,15 +1,19 @@
 class Solution {
+    /*
+    Intuition:
+    - Keep two pointers, one from start index and other from end index
+    - Compute sum of two indexes, if less than target , increment start pointer 
+    - If greater than target, decrement end pointer. Return both indexes when target is found.
+    */
     public int[] twoSum(int[] numbers, int target) {
-        int start = 0; 
-        int end = numbers.length-1;
-        while(start<end){
-            if(numbers[start]+numbers[end]>target){
-                end--;
-            }else if(numbers[start]+numbers[end]<target){
-                start++;
-            }
-            if(numbers[start]+numbers[end]==target){
+        int start = 0, end = numbers.length -1;
+        while(start<=end){
+            if(numbers[start]+numbers[end] == target){
                 return new int[]{start+1,end+1};
+            }else if(numbers[start]+ numbers[end]> target){
+                end--;
+            }else{
+                start++;
             }
         }
         return new int[0];
