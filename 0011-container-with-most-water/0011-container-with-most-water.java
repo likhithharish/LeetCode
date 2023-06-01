d15f25d059402b96875c5d889ccf946ef6890ff4
Likhith Harish <62323090+likhithharish@users.noreply.github.com>
2023-06-01 06:08:53 -0400

    Time: 4 ms (81.33%), Space: 55.2 MB (20.99%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -1,18 +1,22 @@
 class Solution {
+    /*
+    Intuition:
+    - We need to maximise the area. So start with both end points.
+    - calculate the result and keep it to update with max value .
+    - if height of left index is less, increment it , else decrement right . till both meet.
+    */
     public int maxArea(int[] height) {
-        int left = 0;
-        int right = height.length-1;
-        int res = 0;
-        
-        while(left<right){
-            
-            res = Math.max(res, Math.min(height[left],height[right]) * (right-left));
-            if(height[left]<height[right]){
-                left++;
+        int result = 0;
+        int start =0, end = height.length -1;
+        while(start< end){
+            result = Math.max(result, Math.min(height[start],height[end])*(end-start) );
+            if(height[start]<height[end]){
+                start++;
             }else{
-                right--;
+                end--;
             }
         }
-        return res;
+        
+        return result;
     }
 }
