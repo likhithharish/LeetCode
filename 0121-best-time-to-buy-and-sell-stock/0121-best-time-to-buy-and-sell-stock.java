d12fe5651f815efc0e824406dd9f730b4d5ae333
Likhith Harish <62323090+likhithharish@users.noreply.github.com>
2023-06-02 09:07:40 -0400

    Time: 2 ms (93.77%), Space: 61.2 MB (12.53%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,10 +1,16 @@
 class Solution {
+    /*
+    Intuition:
+    - Need to find minimum stock day and highest stock after that.
+    - Loop through and update minStock and also maxProfit.
+    */
     public int maxProfit(int[] prices) {
         int maxProfit = Integer.MIN_VALUE;
         int currMin = prices[0];
         for( int i=0; i< prices.length; i++){
-            maxProfit = Math.max(maxProfit, prices[i] - currMin);
             currMin = Math.min(currMin, prices[i]);
+            maxProfit = Math.max(maxProfit, prices[i] - currMin);
+            
         }
         return maxProfit;
     }
