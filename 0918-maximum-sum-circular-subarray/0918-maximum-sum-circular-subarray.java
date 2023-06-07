9663bb3f56dcbe10b4d296e2c389a0d2df97ac37
Likhith Harish <62323090+likhithharish@users.noreply.github.com>
2023-06-07 09:33:12 -0400

    Time: 3 ms (84.93%), Space: 49.8 MB (16.01%) - LeetHub

--- 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.java
+++ 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.java
@@ -1,27 +1,43 @@
 class Solution {
+    /* Intuition
+    Basic : Consider each eleemnt as first elem and compute sum of all subarrays starting from it(maximum subarray prob). - TLE
+    */
     public int maxSubarraySumCircular(int[] nums) {
-        // NOrmal sum
-        int maxNormal = kadaneAlgo(nums);
-        if(maxNormal<0){ return maxNormal;}
-        
-        //Circular sum - Inverting array(+ to -)
-        // Max sum of Inverted array = min sum of normal arr
-        int arrSum =0;
-        for( int i=0; i<nums.length; i++){
-            arrSum += nums[i];
-            nums[i] = -nums[i];
+        //NAIVE
+        // int res = nums[0];
+        // for (int i = 0; i < nums.length; i++) {
+        //     int currSum = nums[i];
+        //     int currMax = nums[i];
+        //     for (int j = 1; j < nums.length; j++) {
+        //         int index = (i + j) % (nums.length);
+        //         currSum += nums[index];
+        //         currMax = Math.max(currMax, currSum);
+        //     }
+        //     res = Math.max(currMax, res);
+        // }
+        // return res;
+        int normalSum = normalSumSubArray(nums);
+        if (normalSum < 0) {// All elements are negative
+            return normalSum;
         }
-        int circularSum = arrSum + kadaneAlgo( nums);
-        return Math.max(maxNormal,circularSum);
+        int currSum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            currSum += nums[i];
+            nums[i] = -nums[i];
+        } // Min Sum of normal subarray = max sum of inverse sub array
+        int circularSum = currSum + normalSumSubArray(nums);
+        return Math.max(normalSum, circularSum);
     }
     
-    public static int kadaneAlgo(int[] nums) {
-        int res = nums[0];
-        int maxFinding = nums[0];
-        for (int i = 1; i < nums.length; i++) {
-            maxFinding = Math.max(maxFinding + nums[i], nums[i]);
-            res = Math.max(res, maxFinding);
-
+    public static int normalSumSubArray(int[] nums) {
+        int currSum = 0;
+        int res = Integer.MIN_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            currSum += nums[i];
+            res = Math.max(res, currSum);
+            if (currSum < 0) {
+                currSum = 0;
+            }
         }
         return res;
     }
