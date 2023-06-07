97424981b6e3a4852ab3eb8e755e3fddd6f46346
Likhith Harish <62323090+likhithharish@users.noreply.github.com>
2023-06-07 06:37:29 -0400

    Time: 1 ms (97.09%), Space: 44.3 MB (35.73%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,39 +1,14 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        /*Stack<Integer> stack = new Stack<>();
-        int maxProfit = 0;
-
-        for (int i = 0; i < prices.length; i++) {
-            
-            if (stack.isEmpty()) {
-                stack.push(prices[i]);
-                continue;
+        int currMin = prices[0];
+        int res = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] - currMin > 0) {
+                res += prices[i] - currMin;
+                currMin = prices[i];
             }
-            int top = stack.peek();
-            if (stack.size() == 1 && top > prices[i]) {
-                stack.pop();
-                stack.push(prices[i]);
-            } else if (prices[i] > top && stack.size() >= 1) {
-                stack.push(prices[i]);
-            } else if (stack.size() > 1 && prices[i] < top) {
-                maxProfit += stack.peek() - stack.firstElement();
-                stack.clear();
-                stack.push(prices[i]);
-            }
-            
+            currMin = Math.min(currMin, prices[i]);
         }
-        if (!stack.isEmpty() && stack.size() > 1) {
-            maxProfit += stack.peek() - stack.firstElement();
-        }
-
-        return maxProfit;*/
-        int profit = 0;
-        for( int i=1;i< prices.length; i++){
-            if( prices[i]> prices[i-1]){
-                profit += prices[i]- prices[i-1];
-            }
-        }
-         return profit;      
+        return res;
     }
-               
 }
