013225c262961d24bf147c3354fd876b0a26df86
Likhith Harish <62323090+likhithharish@users.noreply.github.com>
2023-06-08 16:28:49 +0530

    Time: 0 ms (100.00%), Space: 41.1 MB (98.64%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -1,20 +1,27 @@
 class Solution {
+    // O(log n) -> binary search
+    //Naive : loop till end and compare neighbours and return if found, handle corners seperately.
+    //Improved : 
     public int findPeakElement(int[] nums) {
-        int low = 0; 
-        int high = nums.length-1;
-        
-        while( low<=high){
-            int mid = (low+high)/2;
-            
-            if((mid==0 || nums[mid-1]<=nums[mid]) && 
-               (mid==nums.length-1 || nums[mid+1]<=nums[mid])){
+        int n = nums.length;
+        if (n == 1 || nums[0] > nums[1]) {
+            return 0;
+        }
+        if (nums[n - 1] > nums[n - 2]) {
+            return n - 1;
+        }
+        int low = 0, high = n - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if ((mid == 0 || nums[mid - 1] <= nums[mid]) && 
+                (mid == n - 1 || nums[mid + 1] <= nums[mid])) {
+                //compare mid with neighbour
                 return mid;
             }
-            
-            if(mid> 0 && nums[mid-1]>=nums[mid]){
-                high = mid -1;
-            }else{
-                low = mid +1;
+            if (mid>0 && nums[mid - 1] >= nums[mid]) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
             }
         }
         return -1;
