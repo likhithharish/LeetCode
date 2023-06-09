8c08f9a85694a1f89494f63d8a183c4bcebaf6d7
Likhith Harish <62323090+likhithharish@users.noreply.github.com>
2023-06-09 13:09:42 +0530

    Time: 5 ms (83.64%), Space: 47 MB (25.31%) - LeetHub

--- 0978-longest-turbulent-subarray/0978-longest-turbulent-subarray.java
+++ 0978-longest-turbulent-subarray/0978-longest-turbulent-subarray.java
@@ -3,35 +3,25 @@ class Solution {
     - Keep track of prev symbol and compare with curr symbol and increment count accord.
     */
     public int maxTurbulenceSize(int[] arr) {
-        if(arr.length==1){
-            return arr.length;
-        }
-        if(arr.length==2){
-            return (arr[0]==arr[1]) ? 1 :2;
-        }
-        int count =0;
-        String sign = "";
-        int res = Integer.MIN_VALUE;
-        for(int i=1; i<arr.length; i++){
-            String temp = sign;
-            if(arr[i]>arr[i-1]){
-                sign =">";
-            }else if(arr[i]<arr[i-1]){
-                sign ="<";
-            }else{
-                sign="";
-                continue;
+        int s = 0, e= 1;
+        int res =1;
+        String prev ="";
+        while(e < arr.length){
+            if( arr[e-1]>arr[e] && prev!=">"){
+                res = Math.max(res, e-s+1);
+                e += 1;
+                prev = ">";
+            }else if( arr[e-1]<arr[e] && prev!="<"){
+                res = Math.max(res, e-s+1);
+                e += 1;
+                prev = "<";
+            }else{//Handling equal sign
+                e = (arr[e]==arr[e-1]) ? e+1 : e;
+                s = e-1;
+                prev = "";
             }
-            if( (temp==">" && sign=="<") || (temp=="<"&& sign==">")){
-                if(count==0){ count+=2;}
-                else{ count+= 1;}
-            }else{
-                count =0;
-            }
-            res = Math.max(res, count);
         }
-        if(res==0){res =1;}
-        if(res == Integer.MIN_VALUE){return 1;}
-    return res+1;
+        return res;
+        
     }
 }
