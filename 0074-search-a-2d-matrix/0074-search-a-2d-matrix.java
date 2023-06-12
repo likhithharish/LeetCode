db8fde33a432930d234e844043df7e71d1097adb
Likhith Harish <62323090+likhithharish@users.noreply.github.com>
2023-06-12 19:12:00 +0530

    Time: 0 ms (100.00%), Space: 41.3 MB (73.06%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,21 +1,34 @@
 class Solution {
     //Find the row and do binary search for element in the row.
+    //O(R+C) - start from top right, if elem is greater- go down else go left
     public boolean searchMatrix(int[][] matrix, int target) {
-        for (int i = 0; i < matrix.length; i++) {
-            int end = matrix[i].length - 1;
-            int start = 0;
-            if (target >= matrix[i][start] && target <= matrix[i][end]) {
-                while (start <= end) {
-                    int mid = (start + end) / 2;
-                    if (matrix[i][mid] == target) {
-                        return true;
-                    }
-                    if (matrix[i][mid] > target) {
-                        end = mid - 1;
-                    } else {
-                        start = mid + 1;
-                    }
-                }
+        // for (int i = 0; i < matrix.length; i++) {
+        //     int end = matrix[i].length - 1;
+        //     int start = 0;
+        //     if (target >= matrix[i][start] && target <= matrix[i][end]) {
+        //         while (start <= end) {
+        //             int mid = (start + end) / 2;
+        //             if (matrix[i][mid] == target) {
+        //                 return true;
+        //             }
+        //             if (matrix[i][mid] > target) {
+        //                 end = mid - 1;
+        //             } else {
+        //                 start = mid + 1;
+        //             }
+        //         }
+        //     }
+        // }
+        // return false;
+        
+        int bottom = 0, left = matrix[0].length - 1;
+        while (left >= 0 && bottom < matrix.length) {
+            if (matrix[bottom][left] == target) {
+                return true;
+            } else if (matrix[bottom][left] > target) {
+                left--;
+            } else {
+                bottom++;
             }
         }
         return false;
