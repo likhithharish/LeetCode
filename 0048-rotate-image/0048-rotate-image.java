ba04432ac3c0b4084708da97999661075ccad20a
Likhith Harish <62323090+likhithharish@users.noreply.github.com>
2023-06-12 13:21:02 +0530

    Time: 0 ms (100.00%), Space: 41 MB (97.90%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.java
@@ -0,0 +1,32 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+       // Reverse,
+        int start = 0, end = matrix.length - 1;
+        while (start <= end) {
+            for (int i = 0; i < matrix[0].length; i++) {
+                int temp = matrix[start][i];
+                matrix[start][i] = matrix[end][i];
+                matrix[end][i] = temp;
+            }
+            start++;
+            end--;
+        }
+        // Transpose
+        matrix = transposeInPlace(matrix);
+    }
+    
+    public int[][] transposeInPlace(int[][] arr) {
+        // Transpose inplace for a square matrix n *n
+        int start = 0;
+        for (int i = 0; i < arr.length; i++) {
+            int end = arr[i].length;
+            for (int j = start; j < end; j++) {
+                int temp = arr[i][j];
+                arr[i][j] = arr[j][i];
+                arr[j][i] = temp;
+            }
+            start++;
+        }
+        return arr;
+    }
+}
