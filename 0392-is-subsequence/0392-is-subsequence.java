4fc8e7ee37561b917db3265c92a8f7e0cac7ab60
Likhith Harish <62323090+likhithharish@users.noreply.github.com>
2023-06-13 17:41:39 +0530

    Time: 1 ms (86.73%), Space: 40.6 MB (72.55%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.java
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -1,4 +1,7 @@
 class Solution {
+    /*
+    Run loop for string s, for each character in s , find if present in t. for next character in s, start from next index in t . whenever not found return false.
+    */
     public boolean isSubsequence(String s, String t) {
         int start = 0;
         boolean found =false;
