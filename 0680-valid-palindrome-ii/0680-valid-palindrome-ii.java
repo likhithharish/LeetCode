73d398cd9d0d3b6b74c4818e153d6acfa2648ddb
Likhith Harish <62323090+likhithharish@users.noreply.github.com>
2023-06-13 11:26:38 +0530

    Time: 10 ms (67.61%), Space: 44.7 MB (15.05%) - LeetHub

---
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean validPalindrome(String s) {
+        int skip =0;
+        int low =0, high =s.length()-1;
+        while(low<=high){
+            if(s.charAt(low)!=s.charAt(high)){
+                if(isPalindrome(s, low+1,high ) || isPalindrome(s,low,high-1)){
+                    return true;
+                }else{
+                    return false;
+                }
+            }
+            low++;
+            high--;
+        }
+        
+        return true;
+    }
+    
+    public boolean isPalindrome(String s, int low, int high){
+        while(low<=high){
+            if(s.charAt(low)!=s.charAt(high)){
+                return false;
+            }
+            low++;
+            high--;
+        }
+        return true;
+    }
+}
