294e22d84c1885ec6c9db35540182e35e6456096
Likhith Harish <62323090+likhithharish@users.noreply.github.com>
2023-06-15 12:38:23 +0530

    Time: 0 ms (100.00%), Space: 41.2 MB (72.51%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.java
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.java
@@ -1,4 +1,5 @@
 class Solution {
+    //Write the scenarios for 1,2,3,4,5 and in two cases u have to go left and rest all u go right.1,2,3,4,5 & 5,1,2,3,4 - left.
     public int findMin(int[] nums) {
         int low =0, high = nums.length -1;
         int res = Integer.MAX_VALUE;
