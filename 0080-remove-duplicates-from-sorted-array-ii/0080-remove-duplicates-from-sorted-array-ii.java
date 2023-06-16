5bee6688f727c68aee339ec87f47fb10fbdb2caf
Likhith Harish <62323090+likhithharish@users.noreply.github.com>
2023-06-16 10:58:36 +0530

    Time: 4 ms (5.94%), Space: 44 MB (17.57%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
@@ -1,36 +1,29 @@
 class Solution {
-    /*
-    Intuition:
-    - Basic : mark the non duplicate elem index in a list , and place the list index values first in temp array and then into nums
-    */
     public int removeDuplicates(int[] nums) {
-        int res =0;
-        List<Integer> list = new ArrayList<>();
-        list.add(0);
-        int curr = nums[0];
-        int count = 1;
-        for( int i=1; i<nums.length; i++){
-            //System.out.println(i+": "+nums[i]+","+curr+","+count);
-            if(nums[i]==curr && count<2){
-                list.add(i);
-                count++;
-            }else if(nums[i]==curr && count>=2){                
+        List<Integer> dups = new ArrayList<>();
+        int count =1;
+        int res = 1;
+        for(int i=1; i<nums.length; i++){
+            if(nums[i]==nums[i-1]){
+                count ++;
                 res++;
+                if(count>2){
+                    dups.add(i);
+                    res -= 1;
+                }
             }else{
-                list.add(i);
-                curr = nums[i];
-                count = 1;
+                count =1;
+                
+                res++;
             }
         }
-        //System.out.println(list);
-        int[] temp = new int[nums.length];
-        for( int i=0; i<list.size(); i++){
-            temp[i] = nums[list.get(i)];
-        }
+        int index = 0;
         for(int i=0; i<nums.length; i++){
-            nums[i] = temp[i];
+            if(!dups.contains(i)){
+                nums[index] = nums[i];
+                index++;
+            }
         }
-        return nums.length - res;
-            
+        return res;
     }
 }
