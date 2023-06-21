18959a3aabbfa996fd9b923513c2f69a7a2eda52
Likhith Harish <62323090+likhithharish@users.noreply.github.com>
2023-06-21 21:31:18 +0530

    Time: 2688 ms (5.74%), Space: 54.5 MB (63.53%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -1,13 +1,11 @@
 class Solution {
     // Use a hashmap and update index in it, if map contains value compute the diff and return true or else update map value.
     public boolean containsNearbyDuplicate(int[] nums, int k) {
-     HashMap<Integer, Integer> map = new HashMap<>();
-        for(int i=0; i<nums.length;i++){
-            if(map.containsKey(nums[i]) && Math.abs(map.get(nums[i])-i)<=k){
-                
-                return true;
-            }else{
-                map.put(nums[i], i);
+        for(int i=0; i<nums.length ; i++){
+            for(int j=i+1; j<=i+k && j<nums.length; j++){
+                if(nums[j]==nums[i] && Math.abs(j-i)<=k){
+                    return true;
+                }
             }
         }
         return false;
