6030abce44382bb76ab2c1734451617e7cfbfb37
Likhith Harish <62323090+likhithharish@users.noreply.github.com>
2023-06-29 05:16:40 +0530

    Time: 20 ms (34.19%), Space: 56.1 MB (43.85%) - LeetHub

---
+++ 1200-minimum-absolute-difference/1200-minimum-absolute-difference.java
@@ -0,0 +1,17 @@
+class Solution {
+    public List<List<Integer>> minimumAbsDifference(int[] arr) {
+        Arrays.sort(arr);
+        int minDif = Integer.MAX_VALUE;
+        for(int i=1; i<arr.length;i++){
+            minDif = Math.min(minDif, Math.abs(arr[i]- arr[i-1]));
+        }
+        List<List<Integer>> res = new ArrayList<>();
+        for(int i=1; i<arr.length; i++){
+            if(Math.abs(arr[i]-arr[i-1])==minDif){
+                List<Integer> temp = Arrays.asList(arr[i-1],arr[i]);
+                res.add(temp);
+            }
+        }
+        return res;
+    }
+}
