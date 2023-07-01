50cfe937fa8c33843778762a81a552e5cdddbf99
Likhith Harish <62323090+likhithharish@users.noreply.github.com>
2023-07-01 20:23:58 +0530

    Time: 4 ms (6.01%), Space: 43.9 MB (44.62%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
@@ -1,30 +1,25 @@
 class Solution {
     //Mark the duplicate indexes in list and update nums if list not contains the index.
     public int removeDuplicates(int[] nums) {
-        List<Integer> dups = new ArrayList<>();
+        List<Integer> indexes = new ArrayList<>();
         int count =1;
-        int res = 1;
         for(int i=1; i<nums.length; i++){
             if(nums[i]==nums[i-1]){
-                count ++;
-                res++;
-                if(count>2){
-                    dups.add(i);
-                    res -= 1;
-                }
+                count++;
             }else{
                 count =1;
-                
-                res++;
+            }
+            if(count>2){
+                indexes.add(i);
             }
         }
-        int index = 0;
-        for(int i=0; i<nums.length; i++){
-            if(!dups.contains(i)){
-                nums[index] = nums[i];
-                index++;
+        int j =0;//two pointer approach
+        for(int i=0;i<nums.length; i++){
+            if(!indexes.contains(i)){
+                nums[j] = nums[i];
+                j++;
             }
         }
-        return res;
+        return nums.length- indexes.size();
     }
 }
