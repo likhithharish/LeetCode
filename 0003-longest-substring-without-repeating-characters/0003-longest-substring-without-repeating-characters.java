767781008f5dd3196ce84be21044f8f69400569c
Likhith Harish <62323090+likhithharish@users.noreply.github.com>
2023-07-04 01:57:32 +0530

    Time: 3 ms (97.12%), Space: 44.2 MB (18.87%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -3,20 +3,16 @@ class Solution {
     - keep a sliging window i(left, j(right). keep track of char index in a map and if char already exists in map , check  if left point is not ahead of prev char index , move left point to new char index +1 and update in map. (corner case - left point check 'abba')
     */
     public int lengthOfLongestSubstring(String s) {
-     HashMap<Character, Integer> map = new HashMap<>();
-        int max = 0;
-        int res =0;
-        int i=0;
-        for(int j=0; j<s.length() && i<s.length(); j++){
-            if(!map.containsKey(s.charAt(j))){
-                map.put(s.charAt(j), j);
-            }else{
-                if(  !(i>map.get(s.charAt(j))) ){
-                    i = map.get(s.charAt(j)) + 1; //move the window left 
-                }
-                map.put(s.charAt(j),j);
-            }
-            res = Math. max(res, j-i+1);
+     int n = s.length();
+        int res = 0;
+        int prev[] = new int[256];
+        Arrays.fill(prev, -1);
+        int i = 0;
+        for (int j = 0; j < n; j++) {
+            i = Math.max(i, prev[s.charAt(j)] + 1);
+            int maxEnd = j - i + 1;
+            res = Math.max(res, maxEnd);
+            prev[s.charAt(j)] = j;
         }
         return res;
     }
