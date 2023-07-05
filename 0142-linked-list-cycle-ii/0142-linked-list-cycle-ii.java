d7a0c544b9854407d9ced0d7ff21b59e7d24abc2
Likhith Harish <62323090+likhithharish@users.noreply.github.com>
2023-07-06 02:18:38 +0530

    Time: 0 ms (100.00%), Space: 43.8 MB (48.59%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
@@ -10,16 +10,32 @@
  * }
  */
 public class Solution {
+    //Method 1 use a hashset.
+    // Method 2 - use flyods effectively.
     public ListNode detectCycle(ListNode head) {
-        HashSet<ListNode> set = new HashSet<>();
-        
+        /*HashSet<ListNode> set = new HashSet<>();
         for(ListNode curr = head; curr !=null; curr = curr.next){
-            if(set.contains(curr)){
-                
-                return curr;
-            }
+            if(set.contains(curr)){ return curr;}
             set.add(curr);
         }
-        return null;
+        return null;*/
+       ListNode slow = head;
+        ListNode fast = head;
+        while (fast!=null && fast.next!=null) { 
+            slow = slow.next; 
+            fast = fast.next.next; 
+            if (slow == fast) { 
+                break; 
+            } 
+        } 
+        if(fast == null || fast.next==null){
+            return null;
+        }
+        slow = head;
+        while(slow != fast){ // not slow.next != fast.next we return the node , not break the loop
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return fast;
     }
 }
