f7f6e87222ace0cead3986ebfc9340001193f467
Likhith Harish <62323090+likhithharish@users.noreply.github.com>
2023-07-06 22:37:26 +0530

    Time: 4 ms (84.96%), Space: 56.6 MB (75.30%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -12,43 +12,46 @@ class Solution {
     // Method 1 : find second half of the list (using two pointers slow, fast) and reverse it. compare first half and second half . if value doesnt match return false.
     // Method 2: use deque and insert all the elem into it and 
     public boolean isPalindrome(ListNode head) {
-        // ListNode slow = head;
-        // ListNode fast = head;
-        // while(fast.next != null && fast.next.next != null){
-        //     slow = slow.next;
-        //     fast = fast.next.next;
-        // }
-        // ListNode rev = reverse(slow.next);
-        // ListNode curr = head;
-        // while (rev != null) {
-        //     if (rev.val != curr.val)
-        //         return false;
-        //     rev = rev.next;
-        //     curr = curr.next;
-        // }
-        // return true;
-        //Method 2
-        Deque<Integer> deque = new ArrayDeque<>();
-        for(ListNode curr = head; curr != null; curr = curr.next){
-            deque.addFirst(curr.val);
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast.next != null && fast.next.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
         }
-        
-        for(ListNode curr = head; curr != null ; curr = curr.next){
-            if(curr.val != deque.removeFirst()){
+        ListNode rev = reverse(slow.next);
+        ListNode curr = head;
+        while (rev != null) {
+            if (rev.val != curr.val)
                 return false;
-            }
+            rev = rev.next;
+            curr = curr.next;
         }
         return true;
+        //Method 2
+//         Deque<Integer> deque = new ArrayDeque<>();
+//         for(ListNode curr = head; curr != null; curr = curr.next){
+//             deque.addFirst(curr.val);
+//         }
+        
+//         for(ListNode curr = head; curr != null ; curr = curr.next){
+//             if(curr.val != deque.removeFirst()){
+//                 return false;
+//             }
+//         }
+//         return true;
     }
     
     
     public ListNode reverse(ListNode head){
-        if (head == null || head.next == null)
-            return head;
-        ListNode rest_head = reverse(head.next);
-        ListNode rest_tail = head.next;
-        rest_tail.next = head;
-        head.next = null;
-        return rest_head;
+         ListNode d = null;
+        
+        while(head!=null)
+        {
+            ListNode nxt = head.next;
+            head.next=d;
+            d = head;
+            head=nxt;
+        }
+        return d;
     }
 }
