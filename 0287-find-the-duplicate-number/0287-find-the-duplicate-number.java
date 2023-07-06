a489032e7b557d6c7c22ddbb0eb702c8eecd0463
Likhith Harish <62323090+likhithharish@users.noreply.github.com>
2023-07-06 23:03:48 +0530

    Time: 5 ms (60.35%), Space: 56.7 MB (56.92%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -2,15 +2,31 @@ class Solution {
     //Increment the freq of elements in a temp array, loop through temp and return any element with freq>1.
     // no extra space - cycle in a LL , slower than above.
     public int findDuplicate(int[] nums) {
-        int[] temp = new int[nums.length];
-        for(int i=0; i<nums.length; i++){
-            temp[nums[i]]++;
+        // int[] temp = new int[nums.length];
+        // for(int i=0; i<nums.length; i++){
+        //     temp[nums[i]]++;
+        // }
+        // for(int i=0;i<temp.length; i++){
+        //     if(temp[i]>1){
+        //         return i;
+        //     }
+        // }
+        // return -1;
+        int slow = nums[0];
+        int fast = nums[0];
+        //Find the candidate
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow != fast);
+        
+        //Verify the candidate
+        slow = nums[0];
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
         }
-        for(int i=0;i<temp.length; i++){
-            if(temp[i]>1){
-                return i;
-            }
-        }
-        return -1;
+        return slow;
+        
     }
 }
