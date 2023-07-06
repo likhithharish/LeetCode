bd526b8597babeb749adcdddb2c070c8249788a0
Likhith Harish <62323090+likhithharish@users.noreply.github.com>
2023-07-06 22:34:56 +0530

    Time: 14 ms (21.49%), Space: 56.3 MB (83.55%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -9,32 +9,38 @@
  * }
  */
 class Solution {
+    // Method 1 : find second half of the list (using two pointers slow, fast) and reverse it. compare first half and second half . if value doesnt match return false.
+    // Method 2: use deque and insert all the elem into it and 
     public boolean isPalindrome(ListNode head) {
-        ListNode slow = head;
-        ListNode fast = head;
-        while(fast.next != null && fast.next.next != null){
-            slow = slow.next;
-            fast = fast.next.next;
+        // ListNode slow = head;
+        // ListNode fast = head;
+        // while(fast.next != null && fast.next.next != null){
+        //     slow = slow.next;
+        //     fast = fast.next.next;
+        // }
+        // ListNode rev = reverse(slow.next);
+        // ListNode curr = head;
+        // while (rev != null) {
+        //     if (rev.val != curr.val)
+        //         return false;
+        //     rev = rev.next;
+        //     curr = curr.next;
+        // }
+        // return true;
+        //Method 2
+        Deque<Integer> deque = new ArrayDeque<>();
+        for(ListNode curr = head; curr != null; curr = curr.next){
+            deque.addFirst(curr.val);
         }
-        ListNode rev = reverse(slow.next);
-        ListNode curr = head;
-        while (rev != null) {
-            if (rev.val != curr.val)
+        
+        for(ListNode curr = head; curr != null ; curr = curr.next){
+            if(curr.val != deque.removeFirst()){
                 return false;
-            rev = rev.next;
-            curr = curr.next;
+            }
         }
         return true;
     }
     
-    public void printList(ListNode head){
-        ListNode curr = head;
-        while(curr!= null){
-            System.out.print(curr.val+" -> ");
-            curr = curr.next;
-        }
-        System.out.println("");
-    }
     
     public ListNode reverse(ListNode head){
         if (head == null || head.next == null)
