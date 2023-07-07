5c2e80c64262f55910bc7a3c800879b6b1039546
Likhith Harish <62323090+likhithharish@users.noreply.github.com>
2023-07-08 00:16:04 +0530

    Time: 1345 ms (5.14%), Space: 46.4 MB (24.42%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.java
+++ 0143-reorder-list/0143-reorder-list.java
@@ -9,27 +9,60 @@
  * }
  */
 class Solution {
+    //Method 1 : Using auxilary space of deque
+    // Method 2: 
     public void reorderList(ListNode head) {
-        Deque<Integer> que = new ArrayDeque<>();
+        // Deque<Integer> que = new ArrayDeque<>();
+        // ListNode curr = head;
+        // while(curr != null){
+        //     que.addLast(curr.val);
+        //     curr = curr.next;
+        // }
+        // que.removeFirst();
+        // curr = head;
+        // int count =0;
+        // while(que.size()!=0){
+        //     if(count%2==0){
+        //         ListNode temp = new ListNode(que.removeLast());
+        //         curr.next = temp;
+        //         curr = temp;
+        //     }else{
+        //         ListNode temp = new ListNode(que.removeFirst());
+        //         curr.next = temp;
+        //         curr = temp;
+        //     }  
+        //     count+=1;
+        // }
+        ListNode curr = head;
+        
+        while(curr!= null && curr.next != null ){
+            ListNode next = curr.next;
+            ListNode last = getLast(head);
+            if(last == next){
+                curr.next = last;
+                break;
+            }
+            curr.next = last;
+            last.next = next;
+            curr = next;
+        }
+    }
+    
+    public void printList(ListNode head){
         ListNode curr = head;
         while(curr != null){
-            que.addLast(curr.val);
+            System.out.print(curr.val+" -> ");
+        }
+        System.out.println("");
+    }
+    
+    public ListNode getLast(ListNode head){
+        ListNode curr = head;
+        while(curr.next.next != null){
             curr = curr.next;
         }
-        que.removeFirst();
-        curr = head;
-        int count =0;
-        while(que.size()!=0){
-            if(count%2==0){
-                ListNode temp = new ListNode(que.removeLast());
-                curr.next = temp;
-                curr = temp;
-            }else{
-                ListNode temp = new ListNode(que.removeFirst());
-                curr.next = temp;
-                curr = temp;
-            }  
-            count+=1;
-        }
+        ListNode last = curr.next;
+        curr.next = null;
+        return last;
     }
 }
