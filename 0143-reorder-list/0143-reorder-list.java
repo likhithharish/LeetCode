275a7ff93e8180c631d378e86443db137d4c58a4
Likhith Harish <62323090+likhithharish@users.noreply.github.com>
2023-07-08 00:39:35 +0530

    Time: 1 ms (100.00%), Space: 46.7 MB (7.24%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.java
+++ 0143-reorder-list/0143-reorder-list.java
@@ -9,9 +9,11 @@
  * }
  */
 class Solution {
-    //Method 1 : Using auxilary space of deque
-    // Method 2: 
+    // Method 1: Using auxilary space of deque
+    // Method 2: Using two while loops , getting last for each new formed list.(more TC)
+    // Method 3: Reverse the second half of linked list and then one node from first half and one node from second half at a time.
     public void reorderList(ListNode head) {
+        //METHOD 1
         // Deque<Integer> que = new ArrayDeque<>();
         // ListNode curr = head;
         // while(curr != null){
@@ -33,25 +35,59 @@ class Solution {
         //     }  
         //     count+=1;
         // }
-        ListNode curr = head;
+        //METHOD 2
+//         ListNode curr = head;
         
-        while(curr!= null && curr.next != null ){
-            ListNode next = curr.next;
-            ListNode last = getLast(head);
-            if(last == next){
-                curr.next = last;
-                break;
-            }
-            curr.next = last;
-            last.next = next;
-            curr = next;
+//         while(curr!= null && curr.next != null ){
+//             ListNode next = curr.next;
+//             ListNode last = getLast(head);
+//             if(last == next){
+//                 curr.next = last;
+//                 break;
+//             }
+//             curr.next = last;
+//             last.next = next;
+//             curr = next;
+//         }
+        //METHOD 3
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast.next!=null && fast.next.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
         }
+        
+        
+        ListNode curr = head;
+        ListNode curr2 = reverseList(slow); // breaks the list into two and reverses second half
+        while(curr2!= null && curr!=null){
+            ListNode next = curr.next;
+            ListNode next2 = curr2.next;
+            curr.next = curr2;
+            curr2.next = next;
+            curr = next;
+            curr2 = next2;
+        }
+    }
+    
+    public ListNode reverseList(ListNode head){
+         ListNode d = null;
+        
+        while(head!=null)
+        {
+            ListNode nxt = head.next;
+            head.next=d;
+            d = head;
+            head=nxt;
+        }
+        return d;
     }
     
     public void printList(ListNode head){
         ListNode curr = head;
         while(curr != null){
             System.out.print(curr.val+" -> ");
+            curr = curr.next;
         }
         System.out.println("");
     }
