63549cf969f04e32b55e2d41565d426d7a5fe263
Likhith Harish <62323090+likhithharish@users.noreply.github.com>
2023-07-09 02:04:03 +0530

    Time: 75 ms (66.67%), Space: 52.6 MB (61.11%) - LeetHub

--- 1063-number-of-valid-subarrays/1063-number-of-valid-subarrays.java
+++ 1063-number-of-valid-subarrays/1063-number-of-valid-subarrays.java
@@ -3,20 +3,34 @@ class Solution {
     // if encounter a value less than that , break it.
     public int validSubarrays(int[] nums) {
         
-        int res =0;
-        for(int i=0; i< nums.length ; i++){
-            res += 1;
-            int tempCount =0;
-            for(int j=i+1; j<nums.length; j++){
-                if(nums[j]>= nums[i]){
-                    tempCount += 1;
-                }else{
-                    break;
-                }
-            }
-            res += tempCount;
-        }
+//         int res =0;
+//         for(int i=0; i< nums.length ; i++){
+//             res += 1;
+//             int tempCount =0;
+//             for(int j=i+1; j<nums.length; j++){
+//                 if(nums[j]>= nums[i]){
+//                     tempCount += 1;
+//                 }else{
+//                     break;
+//                 }
+//             }
+//             res += tempCount;
+//         }
         
-        return res;
+//         return res;
+         int ans = 0;
+        Stack<Integer> monotonicStack = new Stack<>();
+        // use monotonic stack to store index of elements in non-decreasing order
+        for (int i = 0; i < nums.length; i++) {
+            while (!monotonicStack.isEmpty() && nums[monotonicStack.peek()] > nums[i]) {
+                ans += (i - monotonicStack.pop());
+            }
+            monotonicStack.add(i);
+        }
+        // any element remaining in the stack doesn't have a smaller element to the right of it
+        while (!monotonicStack.isEmpty()) {
+            ans += (nums.length - monotonicStack.pop());
+        }
+        return ans;
     }
 }
