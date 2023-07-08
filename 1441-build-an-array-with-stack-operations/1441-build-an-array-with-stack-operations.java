e0b304ac215bb0384ae2c7d3c590fddd0c1002e8
Likhith Harish <62323090+likhithharish@users.noreply.github.com>
2023-07-09 01:51:46 +0530

    Time: 0 ms (100.00%), Space: 42.3 MB (16.99%) - LeetHub

--- 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.java
+++ 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.java
@@ -1,4 +1,5 @@
 class Solution {
+    // loop from 1 to n , for each value of integer, if its present (add push to list) else add (push, pop to list)
     public List<String> buildArray(int[] target, int n) {
         ArrayList<String> list = new ArrayList<>();
         Arrays.sort(target);
