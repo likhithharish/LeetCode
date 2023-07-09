53c1a05ec35288dd1aab0f8a473c153b1400bca2
Likhith Harish <62323090+likhithharish@users.noreply.github.com>
2023-07-09 21:23:48 +0530

    Time: 4 ms (78.64%), Space: 44.1 MB (7.25%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,4 +1,6 @@
 class Solution {
+    // Method 1 - use hashmap to record nums2 ( val,i), then for each val in nums1, find index of it in nums2 using map and get the next greatest value after it.
+    // Method 2 - use stack in addition with map.
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
         HashMap<Integer, Integer> map = new HashMap<>();
         
@@ -6,21 +8,41 @@ class Solution {
             map.put(nums2[i],i);
             
         }
-        int[] ans = new int[nums1.length];
-        Arrays.fill(ans, -1);
-        for(int i=0; i<nums1.length; i++){
-            if(map.containsKey(nums1[i])){
-                for(int j=map.get(nums1[i]); j<nums2.length ; j++){
-                    if(nums2[j]>nums1[i]){
-                        ans[i] = nums2[j];
-                        break;
-                    }
-                }
+        //Part of method 1
+//         int[] ans = new int[nums1.length];
+//         Arrays.fill(ans, -1);
+//         for(int i=0; i<nums1.length; i++){
+//             if(map.containsKey(nums1[i])){
+//                 for(int j=map.get(nums1[i]); j<nums2.length ; j++){
+//                     if(nums2[j]>nums1[i]){
+//                         ans[i] = nums2[j];
+//                         break;
+//                     }
+//                 }
                 
-            }else{
-                ans[i] = -1;
+//             }else{
+//                 ans[i] = -1;
+//             }
+//         }
+//         return ans;
+        //Method 2
+        Stack<Integer> st = new Stack<>();
+        st.push(0);
+        for (int i = 1; i < nums2.length; i++) {
+            while (!st.isEmpty() && nums2[st.peek()] <= nums2[i]) {
+                nums2[st.peek()] = nums2[i];
+                st.pop();
             }
+            st.push(i);
         }
-        return ans;
+        while (!st.isEmpty()) {
+            nums2[st.peek()] = -1;
+            st.pop();
+        }
+        
+        for(int i=0; i< nums1.length; i++){
+            nums1[i] = nums2[map.get(nums1[i])];
+        }
+        return nums1;
     }
 }
