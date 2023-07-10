95ec1f92df5024532b2f2e146b7628e9b4138b22
Likhith Harish <62323090+likhithharish@users.noreply.github.com>
2023-07-10 20:06:40 +0530

    Time: 285 ms (12.54%), Space: 57.6 MB (32.63%) - LeetHub

---
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -0,0 +1,41 @@
+class Solution {
+    // Find leftsmallest indices for each elem and keep in array
+    // rightsmallest indexes for each elem into another key
+    // compute possible area for each elem in heights and update res with maximum.
+    public int largestRectangleArea(int[] heights) {
+        int res = 0;
+        Stack<Integer> st = new Stack<>();
+        st.push(0);
+        int[] ps = new int[heights.length];// stores indexes of previous smallest
+        ps[0] = -1;
+        for (int i = 1; i < heights.length; i++) {
+            while (!st.isEmpty() && heights[st.peek()] > heights[i]) {
+                st.pop();
+            }
+            ps[i] = (st.isEmpty()) ? -1 : st.peek();
+            st.push(i);
+        }
+
+        st.removeAllElements();
+        int[] ns = new int[heights.length];// stores indexes of next smallest
+        st.push(0);
+        for (int i = 1; i < heights.length; i++) {
+            while (!st.isEmpty() && heights[st.peek()] > heights[i]) {
+                ns[st.peek()] = i;
+                st.pop();
+            }
+            st.push(i);
+        }
+        while (!st.isEmpty()) {
+            ns[st.peek()] = heights.length;
+            st.pop();
+        }
+        for (int i = 0; i < heights.length; i++) {
+            int curr = heights[i];
+            curr += (i - ps[i] - 1) * heights[i];
+            curr += (ns[i] - i - 1) * heights[i];
+            res = Math.max(curr, res);
+        }
+        return res;
+    }
+}
