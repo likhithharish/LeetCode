7fe37b607e333ca74551cdcc6712e0a8981193bc
Likhith Harish <62323090+likhithharish@users.noreply.github.com>
2023-07-10 22:45:14 +0530

    Time: 6 ms (61.70%), Space: 47.3 MB (45.48%) - LeetHub

--- 0155-min-stack/0155-min-stack.java
+++ 0155-min-stack/0155-min-stack.java
@@ -1,55 +1,38 @@
-class Node{
-    int data;
-    Node next;
-    Node(int x){
-        data =x;
-    }
-}
 class MinStack {
-    private Node head;
-    private int size;
+    private Stack<Integer> st = new Stack<>();
+    private Stack<Integer> auxSt= new Stack<>();
     
     
     public MinStack() {
-        head = null;
-        size =0;
         
     }
     
     public void push(int val) {
-        Node temp = new Node(val);
-        if(head == null){
-            head = temp;
-           
-            size+=1 ;
-            return;
-        }
-        temp.next = head;
-        head = temp;
-        size +=1 ;
         
+        st.push(val);
+        if(auxSt.isEmpty() || auxSt.peek() >= st.peek()){
+            auxSt.push(val);
+        }
     }
     
     public void pop() {
         
-        Node next = head.next;
-        head.next = null;
-        head = next;
+        if(auxSt.peek().equals(st.peek())){
+            auxSt.pop();
+        }
+        st.pop();
     }
     
     public int top() {
-        return head.data;
+        
+        return st.peek();
     }
     
     public int getMin() {
-       int min = Integer.MAX_VALUE;
-        Node curr = head;
-        while(curr != null){
-            min = Math.min(min, curr.data);
-            curr = curr.next;
-        }
-        return min;
+        return auxSt.peek();
     }
+    
+    
 }
 
 /**
