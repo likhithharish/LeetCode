44141ecdebe4853b308af8f734fa9331c679a755
Likhith Harish <62323090+likhithharish@users.noreply.github.com>
2023-07-10 22:14:52 +0530

    Time: 32 ms (66.17%), Space: 44.3 MB (61.48%) - LeetHub

---
+++ 0085-maximal-rectangle/0085-maximal-rectangle.java
@@ -0,0 +1,36 @@
+class Solution {
+    final int RADIX = 36;
+    public int maximalRectangle(char[][] matrix) {
+        
+        int res = 0;
+        int[] arr = new int[matrix[0].length];
+        for(int i = 0; i < matrix.length; i++) {
+            for(int j = 0; j < matrix[0].length; j++) {
+                arr[j] = matrix[i][j] == '1' ? arr[j] + 1 : 0;
+            }
+            res = Math.max(res, computeHeight(arr));
+        } 
+        return res;
+    }
+    
+    public int computeHeight(int[] heights) {
+        Stack<Integer> st = new Stack<>();
+        int n = heights.length;
+        int res =0;
+        for (int i = 0; i < heights.length; i++) {
+            while (!st.isEmpty() && heights[st.peek()] >=  heights[i]) {
+                int tp = st.pop();
+                int curr = heights[tp] * (st.isEmpty() ? i : (i - st.peek() - 1));
+                res = Math.max(res, curr);
+            }
+            st.push(i);
+        }
+        // Process left over items in stack
+        while (!st.isEmpty()) {
+            int tp = st.pop();
+            int curr =heights[tp] * (st.isEmpty() ? n : (n - st.peek() - 1));
+            res = Math.max(res, curr);
+        }
+        return res;
+    }
+}
