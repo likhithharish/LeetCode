8953c5a8ab46632fd770c5abbae9c60311eab289
Likhith Harish <62323090+likhithharish@users.noreply.github.com>
2023-07-12 20:47:21 +0530

    Time: 6 ms (53.97%), Space: 41.5 MB (44.22%) - LeetHub

--- 1190-reverse-substrings-between-each-pair-of-parentheses/1190-reverse-substrings-between-each-pair-of-parentheses.java
+++ 1190-reverse-substrings-between-each-pair-of-parentheses/1190-reverse-substrings-between-each-pair-of-parentheses.java
@@ -1,4 +1,8 @@
 class Solution {
+    // Use a stack to add every elem while traversing , 
+    //if encountered a ')', remove all elem till next '(' and add it to string and them back to stack from first of string.
+    // at end return the stack as string.
+    
     public String reverseParentheses(String s) {
         Stack<Character> st = new Stack<>();
         for(int i=0; i<s.length(); i++){
