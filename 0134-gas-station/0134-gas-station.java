0e660957869a1b30adcd07bb5b2ec38a8dc64314
Likhith Harish <62323090+likhithharish@users.noreply.github.com>
2023-07-14 03:03:12 +0530

    Time: 26 ms (5.09%), Space: 62.4 MB (5.82%) - LeetHub

--- 0134-gas-station/0134-gas-station.java
+++ 0134-gas-station/0134-gas-station.java
@@ -1,6 +1,7 @@
 class Solution {
     // method 1 (TLE) - Consider each as start and travel until u reach start again while computing curr_gas( ifcurr_gas < 0 break and update start).
-    // Method 2 - 
+    // Method 2 - Use deque (a- Keep adding items to end of deque while curr_gas>=0) (b- keep removing items from front of deque while curr_gas<0)
+    // Corner case of method 2 : curr_gas<0 and deque is empty , make curr_gas =0; run 2*n times to use circular array
     public int canCompleteCircuit(int[] gas, int[] cost) {
         // for (int start = 0; start < gas.length; start++) {
         //     int curr_gas = 0;
@@ -22,7 +23,6 @@ class Solution {
         int curr_gas = 0;
         for (int i = 0; i < 2*n; i++) {
             int k = (i % n);
-            //System.out.println(i+" "+k);
             curr_gas += gas[k] - cost[k];
             if (curr_gas >= 0) {
                 dq.addLast(k);
@@ -31,11 +31,10 @@ class Solution {
                     int index = dq.removeFirst();
                     curr_gas -= (gas[index] - cost[index]);
                 }
-                if(dq.isEmpty() && curr_gas<0){
+                if(dq.isEmpty() && curr_gas<0){//corner case
                     curr_gas =0;
                 }
             }
-            //System.out.println("Dq : " + dq);
             if (dq.size() == n) {
                 return dq.peekFirst();
             }
