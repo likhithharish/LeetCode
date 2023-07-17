934b8c4da216a43d760296d82eb8b3f0ce3cc31e
Likhith Harish <62323090+likhithharish@users.noreply.github.com>
2023-07-18 02:57:07 +0530

    Time: 0 ms (100.00%), Space: 41.3 MB (96.04%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
@@ -14,20 +14,37 @@
  * }
  */
 class Solution {
+    // Method 1 : for every node , compute height of its left subtree and right subtree and do it recursively for every node and return the maximum of it.
+    // Method 2 : compute while computing height.
+    
+    public int res;
     public int diameterOfBinaryTree(TreeNode root) {
-        if(root == null){
-            return 0;
-        }
-        int d1 = height(root.left) + height(root.right);
-        int d2 = diameterOfBinaryTree(root.left);
-        int d3 = diameterOfBinaryTree(root.right);
-        return Math.max(d1, Math.max(d2,d3));
+        // if(root == null){
+        //     return 0;
+        // }
+        // int d1 = height(root.left) + height(root.right);
+        // int d2 = diameterOfBinaryTree(root.left);
+        // int d3 = diameterOfBinaryTree(root.right);
+        // return Math.max(d1, Math.max(d2,d3));
+        
+        //Method 2
+        if(root == null){return 0;}
+        modifiedHeight(root);
+        return res;
     }
     
-    public int height(TreeNode root){
-        if(root == null){
-            return 0;
-        }
-        return Math.max(height(root.left), height(root.right))+1;
+    public int modifiedHeight(TreeNode root){
+        if(root == null){return 0;}
+        int lh = modifiedHeight(root.left);
+        int rh = modifiedHeight(root.right);
+        res = Math.max(res, lh+rh);
+        return 1+Math.max(lh,rh);
     }
+    
+    // public int height(TreeNode root){
+    //     if(root == null){
+    //         return 0;
+    //     }
+    //     return Math.max(height(root.left), height(root.right))+1;
+    // }
 }
