e81b176d81ff01751a673e6ca3ebb1687a2b1c02
Likhith Harish <62323090+likhithharish@users.noreply.github.com>
2023-07-18 07:32:53 +0530

    Time: 14 ms (97.03%), Space: 45 MB (7.34%) - LeetHub

---
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.java
@@ -0,0 +1,58 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+class Solution {
+    // Method 1 : Generate paths for each p and q, return the last common elem in the paths 
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        ArrayList<TreeNode> path1 = new ArrayList<>();
+        ArrayList<TreeNode> path2 = new ArrayList<>();
+        if (!findPath(root, path1, p.val) || !findPath(root, path2, q.val)) {
+            return null;
+        }
+        for(TreeNode t: path1){
+            System.out.print(t.val+" ");
+        }
+        System.out.println(" ");
+        for(TreeNode t: path2){
+            System.out.print(t.val+" ");
+        }
+        for (int i = 0; i < path1.size() - 1 && i < path2.size() - 1; i++) {
+            if (path1.get(i + 1).val != path2.get(i + 1).val) {
+                return path1.get(i);
+            }
+        }
+        if(path1.size()<path2.size()){
+            if(path1.get(path1.size()-1).val == path2.get(path1.size()-1).val){
+                return path1.get(path1.size()-1);
+            }
+        }
+        if(path2.size()< path1.size()){
+            if(path1.get(path2.size()-1).val == path2.get(path2.size()-1).val){
+                return path1.get(path2.size()-1);
+            }
+        }
+        return null;
+    }
+    
+    public boolean findPath(TreeNode root, ArrayList<TreeNode> path, int n) {
+        if (root == null) {
+            return false;
+        }
+        path.add(root);
+        if (root.val == n) {
+            return true;
+        }
+        if (findPath(root.left, path, n) || findPath(root.right, path, n)) {
+            return true;
+        }
+        path.remove(path.size() - 1);
+        return false;
+    }
+}
