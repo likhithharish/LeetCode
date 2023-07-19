02c8c6aa77e98ba61a06fe79acfaac6d9517b748
Likhith Harish <62323090+likhithharish@users.noreply.github.com>
2023-07-20 01:19:39 +0530

    Time: 0 ms (100.00%), Space: 45.8 MB (58.93%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
@@ -16,9 +16,26 @@
 class Solution {
     // Using a recursive sol to count left side and then right side add 1 to count the root of childs.
     public int countNodes(TreeNode root) {
-        if(root == null){
-            return 0;
+        // if(root == null){
+        //     return 0;
+        // }
+        // return countNodes(root.left)+ countNodes(root.right)+1;
+        
+        // Method 2:
+        int lh = 0, rh = 0;
+        TreeNode curr = root;
+        while (curr != null) {
+            lh++;
+            curr = curr.left;
         }
-        return countNodes(root.left)+ countNodes(root.right)+1;
+        curr = root;
+        while (curr != null) {
+            rh++;
+            curr = curr.right;
+        }
+        if (lh == rh) {
+            return (int) (Math.pow(2, lh) - 1);
+        }
+        return countNodes(root.left) + countNodes(root.right) + 1;
     }
 }
