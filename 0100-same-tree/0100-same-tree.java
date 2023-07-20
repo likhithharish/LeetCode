22b4134d03d3916a33102c5457c30dfdc8a10999
Likhith Harish <62323090+likhithharish@users.noreply.github.com>
2023-07-20 08:05:58 +0530

    Time: 0 ms (100.00%), Space: 39.8 MB (90.11%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -14,25 +14,45 @@
  * }
  */
 class Solution {
+    // Method 1 - preOrder traverse and generate Strings and compare them.
+    // Method 2 - dfs - recursion.
     String str;
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        str = "";
-        preOrder(p);
-        String pStr = new String(str);
-        str = "";
-        preOrder(q);
-        return (pStr.equals(str));
+        // str = "";
+        // preOrder(p);
+        // String pStr = new String(str);
+        // str = "";
+        // preOrder(q);
+        // return (pStr.equals(str));
         
+        return dfs(p,q);
     }
     
-    public void preOrder(TreeNode root){
-        if(root != null){
-            str += String.valueOf(root.val) +",";
-            preOrder(root.left);
-            preOrder(root.right);
-        }else{
-            str += "null,";
+    public boolean dfs(TreeNode p, TreeNode q){
+        if (p == null && q == null) {
+            return true;
         }
+
+        if (p == null || q == null) {
+            return false;
+        }
+
+        if (p.val != q.val) return false;
+
+        boolean left = dfs(p.left, q.left);
+        boolean right = dfs(p.right, q.right);
+
+        return left && right;
     }
+    
+    // public void preOrder(TreeNode root){
+    //     if(root != null){
+    //         str += String.valueOf(root.val) +",";
+    //         preOrder(root.left);
+    //         preOrder(root.right);
+    //     }else{
+    //         str += "null,";
+    //     }
+    // }
     
 }
