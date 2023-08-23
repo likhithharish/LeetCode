adde5d67816ca082a30c7ecf7857ea85c6a2eced
Likhith Harish <62323090+likhithharish@users.noreply.github.com>
2023-08-23 13:41:52 -0400

    Time: 1 ms (81.69%), Space: 44.4 MB (82.48%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,26 +1,64 @@
 class Solution {
+    // Naive Impl: Mark the index of zeros an make all elem in its row and col as 0.
     public void setZeroes(int[][] matrix) {
-        List<String> list = new ArrayList<>();
-        for(int i=0; i<matrix.length; i++){
-            for(int j=0; j<matrix[0].length; j++){
-                if(matrix[i][j]==0){
-                    list.add(i+","+j);
+//         List<String> list = new ArrayList<>();
+//         for(int i=0; i<matrix.length; i++){
+//             for(int j=0; j<matrix[0].length; j++){
+//                 if(matrix[i][j]==0){
+//                     list.add(i+","+j);
+//                 }
+//             }
+//         }
+
+//         for(String s: list){
+//             String[] str = s.split(",");
+//             int i= Integer.valueOf(str[0]);
+//             int j = Integer.valueOf(str[1]);
+//             int x =0, y=0;
+//             while(x<matrix.length){
+//                 matrix[x][j] =0;
+//                 x++;
+//             }
+//             while(y<matrix[0].length){
+//                 matrix[i][y] =0;
+//                 y++;
+//             }
+//         }
+        
+        // Approach 2: Mark the first of column and row as 0 , if element is 0. then make that specific row and column 0.
+        boolean firstCol = false;
+        for(int i=0; i< matrix.length; i++){
+            if(matrix[i][0] ==0){
+                firstCol = true;
+            }
+            for(int j=1; j< matrix[0].length; j++){
+                if(matrix[i][j] == 0){
+                    matrix[i][0] =0;
+                    matrix[0][j] =0;
                 }
             }
         }
-
-        for(String s: list){
-            String[] str = s.split(",");
-            int i= Integer.valueOf(str[0]);
-            int j = Integer.valueOf(str[1]);
-            int x =0, y=0;
-            while(x<matrix.length){
-                matrix[x][j] =0;
-                x++;
+        
+        // modify matrix from second row and col
+        for(int i=1; i< matrix.length; i++){
+            for(int j=1; j< matrix[0].length; j++){
+                if(matrix[i][0] ==0 || matrix[0][j]==0){
+                    matrix[i][j] =0;
+                }
             }
-            while(y<matrix[0].length){
-                matrix[i][y] =0;
-                y++;
+        }
+        
+        // modify first row?
+        if(matrix[0][0] ==0){
+            for(int i=0; i< matrix[0].length; i++){
+                matrix[0][i] = 0;
+            }
+        }
+        
+        //modify first col?
+        if(firstCol){
+            for(int i=0;i< matrix.length; i++){
+                matrix[i][0] = 0;
             }
         }
       
