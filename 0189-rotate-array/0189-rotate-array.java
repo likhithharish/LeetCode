096e7ad3b2ffa6d22636567040f86aca22f194a0
Likhith Harish <62323090+likhithharish@users.noreply.github.com>
2023-08-26 11:57:47 -0400

    Time: 1 ms (50.04%), Space: 55.7 MB (61.40%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,8 +1,24 @@
 class Solution {
+    // Method 1: Using extra space
     public void rotate(int[] nums, int k) {
-        int[] temp = Arrays.copyOf(nums, nums.length);
-        for(int i=0; i<nums.length; i++){
-            nums[(i+k)%nums.length] = temp[i];
+        if(nums.length==1){
+            return;
+        }
+        if(k>nums.length){
+            k= (k) % nums.length;
+        }
+        int[] temp = new int[nums.length];
+        int index =0;
+        for(int i=nums.length -k; i< nums.length; i++){
+            temp[index] = nums[i];
+            index++;
+        }
+        for(int i=0; i< nums.length - k; i++){
+            temp[index] = nums[i];
+            index++;
+        }
+        for(int i=0; i< temp.length; i++){
+            nums[i] = temp[i];
         }
     }
 }
