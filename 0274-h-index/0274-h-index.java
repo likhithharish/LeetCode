37934fbf8a973ad91f2cbce5aafe0ad02fd4b015
Likhith Harish <62323090+likhithharish@users.noreply.github.com>
2023-08-26 15:33:54 -0400

    Time: 2 ms (64.37%), Space: 40.7 MB (33.93%) - LeetHub

--- 0274-h-index/0274-h-index.java
+++ 0274-h-index/0274-h-index.java
@@ -1,18 +1,31 @@
 class Solution {
+    
     public int hIndex(int[] citations) {
         Arrays.sort(citations);
-       HashMap<Integer, Integer> map = new HashMap<>();
-        for(int i=0;i<citations.length; i++){
-            if(!map.containsKey(citations[i])){
-                map.put(citations[i], citations.length -i);
+       // HashMap<Integer, Integer> map = new HashMap<>();
+       //  for(int i=0;i<citations.length; i++){
+       //      if(!map.containsKey(citations[i])){
+       //          map.put(citations[i], citations.length -i);
+       //      }
+       //  }
+        //System.out.println(map);
+        int paper = Integer.MIN_VALUE;
+        int res = Integer.MIN_VALUE;
+        for(int i=0; i< citations.length; i++){
+            if(citations[i] != paper){
+                int temp = Math.min(citations[i], citations.length -i);
+                if(temp > res){
+                    res = temp;
+                    paper = citations[i];
+                }
             }
         }
-        //System.out.println(map);
-        int res = Integer.MIN_VALUE;
-        for(Map.Entry<Integer, Integer> e : map.entrySet()){
-            int tempVal = Math.min(e.getKey(), e.getValue());
-            res = Math.max(res, tempVal);
-        }
+        
+        // int res = Integer.MIN_VALUE;
+        // for(Map.Entry<Integer, Integer> e : map.entrySet()){
+        //     int tempVal = Math.min(e.getKey(), e.getValue());
+        //     res = Math.max(res, tempVal);
+        // }
         return res;
     }
 }
