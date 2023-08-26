2948c72cfee923aaee88a450608b8c89038cf12e
Likhith Harish <62323090+likhithharish@users.noreply.github.com>
2023-08-26 15:28:48 -0400

    Time: 4 ms (13.09%), Space: 41.2 MB (7.11%) - LeetHub

---
+++ 0274-h-index/0274-h-index.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int hIndex(int[] citations) {
+        Arrays.sort(citations);
+       HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i=0;i<citations.length; i++){
+            if(!map.containsKey(citations[i])){
+                map.put(citations[i], citations.length -i);
+            }
+        }
+        //System.out.println(map);
+        int res = Integer.MIN_VALUE;
+        for(Map.Entry<Integer, Integer> e : map.entrySet()){
+            int tempVal = Math.min(e.getKey(), e.getValue());
+            res = Math.max(res, tempVal);
+        }
+        return res;
+    }
+}
