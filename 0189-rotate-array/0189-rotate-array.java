ba8f6d73f3487c6413e7e04d0b81e66cf407bd01
Likhith Harish <62323090+likhithharish@users.noreply.github.com>
2023-08-26 12:02:07 -0400

    Time: 0 ms (100.00%), Space: 56.2 MB (30.23%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,24 +1,44 @@
 class Solution {
-    // Method 1: Using extra space
+    // Method 1: Using extra space.
+    // Method 2: Recursion.
     public void rotate(int[] nums, int k) {
-        if(nums.length==1){
-            return;
+        // if(nums.length==1){
+        //     return;
+        // }
+        // if(k>nums.length){
+        //     k= (k) % nums.length;
+        // }
+        // int[] temp = new int[nums.length];
+        // int index =0;
+        // for(int i=nums.length -k; i< nums.length; i++){
+        //     temp[index] = nums[i];
+        //     index++;
+        // }
+        // for(int i=0; i< nums.length - k; i++){
+        //     temp[index] = nums[i];
+        //     index++;
+        // }
+        // for(int i=0; i< temp.length; i++){
+        //     nums[i] = temp[i];
+        // }
+        
+        //Approach 2: Recursion:
+        if(k > nums.length){
+         k = k % nums.length;   
         }
-        if(k>nums.length){
-            k= (k) % nums.length;
+        reverse(nums, 0, nums.length-k-1);
+        reverse(nums, nums.length-k, nums.length-1);
+        reverse(nums, 0, nums.length-1);
+    }
+    
+    public static int[] reverse(int[] nums, int start, int end){
+        while(start< end){
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
         }
-        int[] temp = new int[nums.length];
-        int index =0;
-        for(int i=nums.length -k; i< nums.length; i++){
-            temp[index] = nums[i];
-            index++;
-        }
-        for(int i=0; i< nums.length - k; i++){
-            temp[index] = nums[i];
-            index++;
-        }
-        for(int i=0; i< temp.length; i++){
-            nums[i] = temp[i];
-        }
+        return nums;
     }
 }
