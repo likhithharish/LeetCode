aa384ac45b7109de61b233fb2c6639131f085fff
Likhith Harish <62323090+likhithharish@users.noreply.github.com>
2023-08-27 13:39:30 -0400

    Time: 3 ms (79.91%), Space: 44.9 MB (73.03%) - LeetHub

---
+++ 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.java
@@ -0,0 +1,20 @@
+class Solution {
+    // Intuition: generate number from num array, compute sum and it to a list , reverse the list.
+    public List<Integer> addToArrayForm(int[] num, int k) {
+        List<Integer> res = new ArrayList<>();
+        int carry =0;
+        int i = num.length -1;
+        while(i>=0 || k>0 || carry>0 ){
+            int val = (i>=0) ? num[i--] : 0;
+            
+            int sum = val + (k%10) + carry;
+            res.add(sum %10);
+            carry = sum /10;
+            k = k/10;
+        }
+        
+        Collections.reverse(res);
+        
+        return res;
+    }
+}
