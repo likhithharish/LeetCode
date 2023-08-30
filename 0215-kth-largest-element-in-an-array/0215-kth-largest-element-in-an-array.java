d7e537f416a8d58ee65665678a8f5e4c3dfc49e2
Likhith Harish <62323090+likhithharish@users.noreply.github.com>
2023-08-30 02:03:23 -0400

    Time: 56 ms (32.18%), Space: 57.2 MB (11.79%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
@@ -1,12 +1,21 @@
 class Solution {
     // Appraoch 1 : Sort and return (n-k)th element. (Not efficient) (nlogn)
     // Approach 2: Quick select (cant use arrays directly, modify to list to reduce)
+    // Approach 3: using Min heap - add each num and if size is more than k , remove first, finally return the peek. 
     public int findKthLargest(int[] nums, int k) {
-       List<Integer> vals = new ArrayList<>();
+       // List<Integer> vals = new ArrayList<>();
+       //  for(int i: nums){
+       //      vals.add(i);
+       //  }
+       //  return quickSelect(vals, k);
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
         for(int i: nums){
-            vals.add(i);
+            pq.add(i);
+            if(pq.size() > k){
+                pq.poll();
+            }
         }
-        return quickSelect(vals, k);
+        return pq.peek();
     }
     
     public int quickSelect(List<Integer> nums, int k){
