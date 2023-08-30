502bc8693c0fc1b6712f647c8b03e2ab4f82cf05
Likhith Harish <62323090+likhithharish@users.noreply.github.com>
2023-08-30 02:12:24 -0400

    Time: 276 ms (5.17%), Space: 56.1 MB (29.86%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
@@ -1,21 +1,22 @@
 class Solution {
     // Appraoch 1 : Sort and return (n-k)th element. (Not efficient) (nlogn)
     // Approach 2: Quick select (cant use arrays directly, modify to list to reduce)
-    // Approach 3: using Min heap - add each num and if size is more than k , remove first, finally return the peek. 
+    // Approach 3: using Min heap - add each num and if size is more than k , remove first, finally return the peek.
+    
     public int findKthLargest(int[] nums, int k) {
-       // List<Integer> vals = new ArrayList<>();
-       //  for(int i: nums){
-       //      vals.add(i);
-       //  }
-       //  return quickSelect(vals, k);
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
+       List<Integer> vals = new ArrayList<>();
         for(int i: nums){
-            pq.add(i);
-            if(pq.size() > k){
-                pq.poll();
-            }
+            vals.add(i);
         }
-        return pq.peek();
+        return quickSelect(vals, k);
+        // PriorityQueue<Integer> pq = new PriorityQueue<>();
+        // for(int i: nums){
+        //     pq.add(i);
+        //     if(pq.size() > k){
+        //         pq.poll();
+        //     }
+        // }
+        // return pq.peek();
     }
     
     public int quickSelect(List<Integer> nums, int k){
@@ -28,21 +29,20 @@ class Solution {
         
         for(int i: nums){
             if(i > pivot){
-                left.add(i);
-            }else if( i< pivot){
                 right.add(i);
+            }else if( i< pivot){
+                left.add(i);
             }else{
                 mid.add(i); // duplicates to pivot value
             }
         }
         
-        if(k <= left.size()){
-            return quickSelect(left, k);
+        if(k <= right.size()){
+            return quickSelect(right, k);
         }
         
-        
-        if(left.size() + mid.size() < k){
-            return quickSelect(right, k - left.size() - mid.size());
+        if(right.size() + mid.size() < k){
+            return quickSelect(left, k - right.size() - mid.size());
         }
         return pivot;
     }
