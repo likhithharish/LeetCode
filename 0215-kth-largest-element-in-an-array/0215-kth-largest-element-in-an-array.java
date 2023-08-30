c5ecf8f8b2bd879cd2c1ce9b66744d112c6aee01
Likhith Harish <62323090+likhithharish@users.noreply.github.com>
2023-08-30 01:56:58 -0400

    Time: 277 ms (5.17%), Space: 55.8 MB (35.94%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
@@ -1,7 +1,40 @@
 class Solution {
-    // Appraoch 1 : Sort and return (n-k)th element. ()
+    // Appraoch 1 : Sort and return (n-k)th element. (Not efficient) (nlogn)
+    // Approach 2: Quick select (cant use arrays directly, modify to list to reduce)
     public int findKthLargest(int[] nums, int k) {
-        Arrays.sort(nums);
-        return nums[nums.length-k];
+       List<Integer> vals = new ArrayList<>();
+        for(int i: nums){
+            vals.add(i);
+        }
+        return quickSelect(vals, k);
+    }
+    
+    public int quickSelect(List<Integer> nums, int k){
+        int p =  nums.size()-1;
+        int pivot = nums.get(p);
+        
+        List<Integer> left = new ArrayList<>();
+        List<Integer> right = new ArrayList<>();
+        List<Integer> mid = new ArrayList<>();
+        
+        for(int i: nums){
+            if(i > pivot){
+                left.add(i);
+            }else if( i< pivot){
+                right.add(i);
+            }else{
+                mid.add(i); // duplicates to pivot value
+            }
+        }
+        
+        if(k <= left.size()){
+            return quickSelect(left, k);
+        }
+        
+        
+        if(left.size() + mid.size() < k){
+            return quickSelect(right, k - left.size() - mid.size());
+        }
+        return pivot;
     }
 }
