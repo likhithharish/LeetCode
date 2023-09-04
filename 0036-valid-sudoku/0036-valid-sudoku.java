51ac0506abda38fce7d5218c45395f323a8a0e1f
Likhith Harish <62323090+likhithharish@users.noreply.github.com>
2023-09-04 14:20:05 -0400

    Time: 3 ms (64.98%), Space: 43.1 MB (82.19%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,35 +1,31 @@
 class Solution {
-    /*Intuition:
-    - maintain three hashset of int[] arrays, loop through grid in n2 time. add row wise elem to rows set , col wise to cols set and for squares , treat each square as index 0 to 9: compute index = (i/3)*3 + ( j / 3)
-    */
+    // keep track of visited elements in rows, cols, and each square,(array of hashsets) return false if element exists.
     public boolean isValidSudoku(char[][] board) {
         Set<Character> rows[] = new Set[9];
         Set<Character> cols[] = new Set[9];
-        Set<Character> squares[] = new Set[9];
-
-        for (int i = 0; i < 9; i++) {
+        Set<Character> sqrs[] = new Set[9];
+        
+        for(int i=0; i<9; i++){
             rows[i] = new HashSet<>();
             cols[i] = new HashSet<>();
-            squares[i] = new HashSet<>();
+            sqrs[i] = new HashSet<>();
         }
         
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
+        for(int i=0; i<9; i++){
+            for(int j=0; j<9; j++){
                 char c = board[i][j];
-                if (c == '.') {
+                if(c== '.'){
                     continue;
                 }
-
-                if (rows[i].contains(c) || 
-                    cols[j].contains(c) || 
-                    squares[(i / 3) * 3 + j / 3].contains(c)
-                    ) {
-                        return false;
+                
+                if(rows[i].contains(c)|| cols[j].contains(c) ||
+                    sqrs[(i/3)*3 + j/3].contains(c) ){
+                    return false;
                 }
-
+                
                 rows[i].add(c);
                 cols[j].add(c);
-                squares[(i / 3) * 3 + j / 3].add(c);
+                sqrs[(i/3)*3 + j/3].add(c);
             }
         }
         
