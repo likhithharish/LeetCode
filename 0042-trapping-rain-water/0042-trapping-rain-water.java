d2594beaef7bc54f163c423ccf5795e1237e4bdc
Likhith Harish <62323090+likhithharish@users.noreply.github.com>
2023-09-06 04:43:09 -0400

    Time: 1 ms (82.55%), Space: 44.4 MB (51.95%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,21 +1,20 @@
 class Solution {
-    /*
-    Intuition:
-    - At a given index, u can fill water to a max height (specified by its boundary either on left or right maximum value) - its value.
-    */
     public int trap(int[] height) {
         int[] leftMax = new int[height.length];
-        leftMax[0] = height[0];
-        for (int i = 1; i < height.length; i++) {
-            leftMax[i] = Math.max(height[i], leftMax[i - 1]);
+        leftMax[0] = -1;
+        for(int i=1; i<leftMax.length; i++){
+            leftMax[i] = Math.max(height[i-1], leftMax[i-1]);
         }
-        int rightMax = height[height.length - 1];
-        int res = 0;
-        for (int i = height.length - 2; i >= 0; i--) {
-            int val = Math.min(leftMax[i], rightMax) - height[i];
-            res += (val > 0) ? val : 0;
-            rightMax = Math.max(height[i], rightMax);
+        
+        int rightMax = -1;
+        int ans = 0;
+        for(int i=leftMax.length-1; i>=0; i--){
+            if(Math.min(leftMax[i], rightMax) - height[i] >0){
+                ans += Math.min(leftMax[i], rightMax) - height[i];
+            }
+            rightMax = Math.max(rightMax, height[i]);
+            
         }
-        return res;
+        return ans;
     }
 }
