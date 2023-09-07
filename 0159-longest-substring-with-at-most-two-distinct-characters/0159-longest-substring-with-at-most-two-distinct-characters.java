5633517408f11ba764c760e5746dba6e670fa668
Likhith Harish <62323090+likhithharish@users.noreply.github.com>
2023-09-07 11:38:21 -0400

    Time: 81 ms (17.58%), Space: 44.2 MB (70.76%) - LeetHub

--- 0159-longest-substring-with-at-most-two-distinct-characters/0159-longest-substring-with-at-most-two-distinct-characters.java
+++ 0159-longest-substring-with-at-most-two-distinct-characters/0159-longest-substring-with-at-most-two-distinct-characters.java
@@ -1,12 +1,11 @@
 class Solution {
-    // Sliding Window + Hash : 
+    // Sliding Window + Hash : Update each character and index(second pointer) in hashmap, when hashmap exceeds length 2, remove the min index based char in map and update first pointer as index+1, compute result every time for each window length.
     public int lengthOfLongestSubstringTwoDistinct(String s) {
         HashMap<Character, Integer> map = new HashMap<>();
         int i=0;
         int res =0;
         for(int j=0; j<s.length() && i<s.length(); j++){
             map.put(s.charAt(j),j);
-            
             if(map.size()==3){
                 int index = Collections.min(map.values());
                 map.remove(s.charAt(index));
