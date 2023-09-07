12cbf954430f74f06805514f22bc0eb0922f0071
Likhith Harish <62323090+likhithharish@users.noreply.github.com>
2023-09-07 11:41:43 -0400

    Time: 1 ms (100.00%), Space: 41.6 MB (87.83%) - LeetHub

--- 0340-longest-substring-with-at-most-k-distinct-characters/0340-longest-substring-with-at-most-k-distinct-characters.java
+++ 0340-longest-substring-with-at-most-k-distinct-characters/0340-longest-substring-with-at-most-k-distinct-characters.java
@@ -1,17 +1,28 @@
 class Solution {
     public int lengthOfLongestSubstringKDistinct(String s, int k) {
-        HashMap<Character, Integer> map = new HashMap<>();
-        int i=0;
-        int res =0;
-        for(int j=0; j<s.length() && i<s.length(); j++){
-            map.put(s.charAt(j),j);
-            if(map.size()> k){
-                int index = Collections.min(map.values());
-                map.remove(s.charAt(index));
-                i = index +1;
-            }
-            res = Math.max(res, j-i+1);
+        // HashMap<Character, Integer> map = new HashMap<>();
+        // int i=0;
+        // int res =0;
+        // for(int j=0; j<s.length() && i<s.length(); j++){
+        //     map.put(s.charAt(j),j);
+        //     if(map.size()> k){
+        //         int index = Collections.min(map.values());
+        //         map.remove(s.charAt(index));
+        //         i = index +1;
+        //     }
+        //     res = Math.max(res, j-i+1);
+        // }
+        // return res;
+        
+        int[] arr = new int[256];
+        char[] ch = s.toCharArray();
+        int left=0,ans=0,count=0;
+        for(int i=0;i<ch.length;i++){
+            if (++arr[ch[i]]==1) count++;
+            while(count>k) 
+				if (--arr[ch[left++]]==0) count--;
+            ans = Math.max(ans,i-left+1);
         }
-        return res;
+        return ans;
     }
 }
