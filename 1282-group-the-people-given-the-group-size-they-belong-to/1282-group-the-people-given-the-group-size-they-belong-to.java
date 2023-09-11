c7486ff3c450ee88e86b2cfbafee1f3871d98ef5
Likhith Harish <62323090+likhithharish@users.noreply.github.com>
2023-09-11 13:54:14 -0400

    Time: 6 ms (83.58%), Space: 44.4 MB (70.67%) - LeetHub

--- 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.java
+++ 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.java
@@ -1,38 +1,49 @@
 class Solution {
+    // Basic Intuiton: Add to hashmap size, list. split the list if listsize > size and add it to res.
     public List<List<Integer>> groupThePeople(int[] groupSizes) {
         List<List<Integer>> res = new ArrayList<>();
         HashMap<Integer, List<Integer>> map = new HashMap<>();
         
         for(int i=0; i<groupSizes.length; i++){
-            if(map.containsKey(groupSizes[i]) ){
-                List<Integer> temp = map.get( groupSizes[i] );
-                temp.add(i);
-                map.put(groupSizes[i] , temp);
-            }else{
-                map.put(groupSizes[i], new ArrayList<>(Arrays.asList(i)));
+            // if(map.containsKey(groupSizes[i]) ){
+            //     List<Integer> temp = map.get( groupSizes[i] );
+            //     temp.add(i);
+            //     map.put(groupSizes[i] , temp);
+            // }else{
+            //     map.put(groupSizes[i], new ArrayList<>(Arrays.asList(i)));
+            // }
+            if(!map.containsKey(groupSizes[i])){
+                     map.put(groupSizes[i], new ArrayList<>());
+            }
+            List<Integer> groups = map.get(groupSizes[i]);
+            groups.add(i);
+            
+            if(groups.size() == groupSizes[i]){
+                res.add(groups);
+                map.remove(groupSizes[i]);
             }
         }
         
-        for(Map.Entry<Integer, List<Integer>> e: map.entrySet()){
-            if(e.getKey() == e.getValue().size()){
-                res.add(e.getValue());
-            }else{
-                int size = e.getKey();
-                List<Integer> temp = e.getValue();
-                int count = 0;
-                List<Integer> out = new ArrayList<>();
-                while(count != size && !temp.isEmpty()){
-                    int val = temp.remove(temp.size()-1);
-                    count++;
-                    out.add(val);
-                    if(count == size){
-                        res.add(out);
-                        out = new ArrayList<>();
-                        count = 0;
-                    }
-                }
-            }
-        }
+        // for(Map.Entry<Integer, List<Integer>> e: map.entrySet()){
+        //     if(e.getKey() == e.getValue().size()){
+        //         res.add(e.getValue());
+        //     }else{
+        //         int size = e.getKey();
+        //         List<Integer> temp = e.getValue();
+        //         int count = 0;
+        //         List<Integer> out = new ArrayList<>();
+        //         while(count != size && !temp.isEmpty()){
+        //             int val = temp.remove(temp.size()-1);
+        //             count++;
+        //             out.add(val);
+        //             if(count == size){
+        //                 res.add(out);
+        //                 out = new ArrayList<>();
+        //                 count = 0;
+        //             }
+        //         }
+        //     }
+        // }
         return res;
         
     }
